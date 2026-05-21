82c609c829101e0d0e582afcdf23238054150f09
Kathy Phan <101038166+Kathy-Phan@users.noreply.github.com>
2026-05-21 21:58:42 +1000

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,9 +3,10 @@ import sys
 
 def main():
     # TODO: Uncomment the code below to pass the first stage
-    sys.stdout.write("$ ")
-    user_input = input()
-    print(f"{user_input}: command not found")
+    while 1: 
+        sys.stdout.write("$ ")
+        user_input = input()
+        print(f"{user_input}: command not found")
     pass
 
 
