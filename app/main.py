e8ea6246d1b4c99f762a9dd9deac558803d33f7a
Kathy Phan <101038166+Kathy-Phan@users.noreply.github.com>
2026-06-04 12:41:24 +1000

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,13 +2,16 @@ import sys
 
 
 def main():
-    # TODO: Uncomment the code below to pass the first stage
     while 1: 
         sys.stdout.write("$ ")
         user_input = input()
-        print(f"{user_input}: command not found")
-    pass
+        if user_input == "exit":
+            break
 
+        print(f"{user_input}: command not found")
+
+        
+    pass
 
 if __name__ == "__main__":
     main()
