77464fa651482c77c80ee773622d82567f75da34
Kathy Phan <101038166+Kathy-Phan@users.noreply.github.com>
2026-06-04 13:39:03 +1000

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -7,8 +7,18 @@ def main():
         user_input = input()
         if user_input == "exit":
             break
+        elif "echo" in user_input:
+            print(user_input[5:])
+        else:
+            print(f"{user_input}: command not found")
 
-        print(f"{user_input}: command not found")
+        # match user_input:
+        #     case "exit":
+        #         break
+        #     case "echo":
+        #         print(f"{user_input}")
+        #     case _:
+        #         print(f"{user_input}: command not found")
 
         
     pass
