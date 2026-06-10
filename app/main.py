1533981beae0448beb62e629bbcafec7c4b9af5f
Kathy Phan <101038166+Kathy-Phan@users.noreply.github.com>
2026-06-10 13:50:45 +1000

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,24 +1,33 @@
-import sys, shutil
+import sys, shutil, subprocess
 
 def main():
+    builtin_commands = ["exit", "echo", "type"];
+
     while 1: 
         sys.stdout.write("$ ")
+
         user_input = input()
         cmd = user_input[5:]
         file_path = shutil.which(cmd)
-        builtin_commands = ["exit", "echo", "type"];
-
+        
+        program_name = user_input.split()[0]
+        arguments = user_input.split()[1:]
+        
         if user_input == "exit":
             break
         elif user_input.startswith("echo"):
             print(cmd)
         elif user_input.startswith("type"):
+            # if command is built-in
             if cmd in builtin_commands:
                 print(f'{cmd} is a shell builtin')
             elif file_path:
                 print(f'{cmd} is {file_path}')
             else: 
                 print(f'{cmd}: not found')
+        elif shutil.which(program_name):
+            res = subprocess.run([program_name] + arguments, capture_output=True, text=True)
+            print(res.stdout, end='')
         else: 
             print(f'{user_input}: command not found')
     pass
