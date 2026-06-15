746474b9c8d462ec8fd8e6fd9f569985bb8c1d5a
Kathy Phan <101038166+Kathy-Phan@users.noreply.github.com>
2026-06-15 17:59:04 +1000

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -15,20 +15,40 @@ def main():
 
         program_name = tokens[0]
         arguments = tokens[1:]
+            
+        if '>' in tokens or '1>' in tokens:
+            symbol = '>' if '>' in tokens else '1>'
+            redirect_symbol = tokens.index(symbol)
+            cmd = tokens[:redirect_symbol]
+            result = subprocess.run(cmd, capture_output=True, text=True)
+            redirect(result, tokens[redirect_symbol + 1])
+            continue
+       
+        if program_name not in builtin_commands: 
+            if shutil.which(program_name):
+                res = subprocess.run([program_name] + arguments, capture_output=True, text=True)
+                print(res.stdout, end='')
+                if res.stderr:
+                    print(res.stderr, end='')
+            else:
+                print_command_not_found(user_input)
+            continue
 
         match program_name:
             case "exit": exit(0)
-            case "echo": print(" ".join(arguments))
+            case "echo": print(" ".join(arguments) )
             case "type":
-                locate_executable_file(" ".join(arguments))
+                locate_executable_file(" ".join(arguments) )
             case "pwd": print(os.getcwd())
             case "cd":
-                change_dir(" ".join(arguments))
-            case _ if shutil.which(program_name): 
-                res = subprocess.run([program_name] + arguments, capture_output=True, text=True)
-                print(res.stdout, end='')
-            case _ : print(f'{user_input}: command not found')
+                change_dir(" ".join(arguments) )
+            case _ : print_command_not_found(user_input)
+
+        
     pass 
+
+def print_command_not_found(user_input):
+    print(f'{user_input}: command not found')
 
 def locate_executable_file(file):
     is_executable = shutil.which(file)
@@ -48,6 +68,13 @@ def change_dir(dir):
     else: 
         print(f'cd: {dir}: No such file or directory')
 
+def redirect(res, file):
+    os.makedirs(os.path.dirname(file), exist_ok=True)
+    with open(file, "w") as f:
+        f.write(res.stdout)
+        
+    if res.stderr:
+        print(res.stderr, end='')
 
 if __name__ == "__main__":
     main()
